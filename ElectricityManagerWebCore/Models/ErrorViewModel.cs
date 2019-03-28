1a97a396ef05d2aa9b1ca5b2064a9a9cdbe0d135
vinogradovnick32 <vinogradovnick32@gmail.com>
2019-03-28 22:30:10 +0500

    init backend project

---
+++ ElectricityManagerWebCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElectricityManagerWebCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
