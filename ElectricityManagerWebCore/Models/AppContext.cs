7ae1ee8dcbbe9f8a5282b7b229c551ef84c208e0
vinogradnick <vinogradovnick32@gmail.com>
2019-03-31 10:56:10 +0500

    add new models

--- ElectricityManagerWebCore/Models/AppContext.cs
+++ ElectricityManagerWebCore/Models/AppContext.cs
@@ -5,6 +5,11 @@ namespace ElectricityManagerWebCore.Models
     public class AppContext:DbContext
     {
         public DbSet<Fider> Fiders { get; set; }
+        public DbSet<Tp> Tps { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Line> Lines { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Workload> Workloads { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=32768;Database=usersdb;Username=postgres;");
